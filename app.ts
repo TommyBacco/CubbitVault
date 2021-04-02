8cfee9306bc6f4f405292ee5a14172ceee933d6a
Tommaso Bacconi <bacco@cryptolab.net>
2021-04-02 15:24:38 +0200

    store file in upload

--- app.ts
+++ app.ts
@@ -1,11 +1,26 @@
 const express = require('express');
-var cors = require('cors'); 
+const cors = require('cors'); 
+const multer = require('multer');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
-app.use(cors()); 
+const upload = multer({dest: 'uploads/'});
 const PORT = 5000;
+
+app.use(cors()); 
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 //support parsing of application/json type post data
-const myNotes : string[] = []
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, '/src/files');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname);
+  }
+});
 
 app.use(function(req, res, next) { 
    res.header("Access-Control-Allow-Origin", "*"); 
@@ -25,9 +40,9 @@ app.listen(PORT, () => {
 });
 
 
-app.post('/note', (req, res) => {
-	myNotes.push(req.body)
-	console.log(myNotes)
-  // and further:
-  res.send("received")
+app.post('/v1/files', upload.single("file") ,(req, res, next) => {
+	if(!req.file){console.log("fallito")}else{console.log("ricevuto " + req.file.fieldname)}
+	//fileStorage.push(req.file)
+
+	next()
 });
