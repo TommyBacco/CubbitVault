5c6d9a438d24a96cd05a28971b89a317956075f4
Tommaso Bacconi <bacco@cryptolab.net>
2021-04-03 18:25:18 +0200

    all up and running

--- app.ts
+++ app.ts
@@ -48,15 +48,36 @@ app.listen(PORT, () => {
   console.log('⚡️[server]: Server is running at https://localhost:' + PORT);
 });
 
+app.get('/v1/data/', (req,res)=>{
+
+  const readMetadata = "SELECT * FROM FilesTable WHERE UUID = unhex(replace(?,'-',''))"
+  db.query(readMetadata, [req.header("uuid")], (err, response)=>{
+    if(err){response.send(err)}else {
+      console.log(response)
+      res.send({
+        'filename':response[0].Path,
+        'mime':response[0].Mime,
+        'size':response[0].Size
+      })
+    }
+  })
+})
+
 app.get('/v1/files', (req,res)=> {
 	res.set({
 	  "Access-Control-Expose-Headers": "Content-Disposition",
 	  'Content-Disposition': 'attachment',
 	  'filename':"path"
 	})
-	var filePath = "./upload/blob"
-	var resolvedPath = path.resolve(filePath);
-	res.sendFile(resolvedPath)})
+  const readDb = "SELECT * FROM FilesTable WHERE UUID = unhex(replace(?,'-',''))"
+  db.query(readDb, [req.header("uuid")], (err, response)=>{
+    if(err){response.send(err)}else{
+      var filePath = "./upload/" + response[0]['Path'] + '.enc'
+      var resolvedPath = path.resolve(filePath);
+      res.sendFile(resolvedPath)
+    }
+  })
+	})
 
 
 app.post('/v1/files', upload.single("file") ,(req, res, next) => {
